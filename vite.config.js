2d8e9c3a7c46066d620cfc3699e42a838a3a69a4
Azira25 <azisnewer@gmail.com>
2025-12-02 15:05:12 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // GANTI 'nama-repo-anda' dengan nama repository GitHub yang Anda buat
   // Contoh: jika repo anda namanya 'youtube-clipper', maka: base: '/youtube-clipper/'
-  base: '/nama-repo-anda/', 
+  base: '/Ai-Clipping/', 
 })
